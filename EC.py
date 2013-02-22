7b7fb3f8fc1367fde321b8f7d570489870927fe8
Diogo Silva <o.diogosilva@gmail.com>
2013-02-22 18:24:36 +0000

    Implemented first elements of menu bar and exit events

--- EC.py
+++ EC.py
@@ -22,11 +22,81 @@
 #  
 #  
 
+import sys
+from PyQt4 import QtGui, QtCore
 
+class EC (QtGui.QMainWindow):
+
+	def __init__(self):
+		super(EC, self).__init__()
+        
+		self.initUI()
+        
+	def initUI(self): 
+    
+		## Geometry
+		self.setGeometry(300, 300, 290, 150)
+		self.center()
+		## Header
+		self.setWindowTitle('ElConcatenero')   
+		#self.setWindowIcon(QtGui.QIcon('sombrero.png')) # ICON MISSING HERE
+        
+		# Menu bar
+		self.createMenus()
+        
+        
+		# Status bar
+
+	def createMenus (self):
+		""" Creation of the menus for the menu bar """
+		# FILE MENU
+		openFile = QtGui.QAction(QtGui.QIcon('open.png'), 'Open', self)
+		openFile.setShortcut('Ctrl+O')
+		openFile.setStatusTip('Open new File')
+		#openFile.triggered.connect(self.showDialog)
+        
+		menubar = self.menuBar()
+		fileMenu = menubar.addMenu('&File')
+		fileMenu.addAction(openFile)
+		
+		# MODE MENU
+		
+		concatenation = QtGui.QAction(QtGui.QIcon('open.png'),'Concatenation',self)
+		concatenation.setShortcut('Ctrl+Alt+C')
+		concatenation.setStatusTip('Opens the concatenation window')
+		
+		modeMenu = menubar.addMenu('&Mode')
+		modeMenu.addAction(concatenation)
+			
+	def center(self):
+		""" Function used to center the main window in the Desktop environment """
+		qr = self.frameGeometry()
+		cp = QtGui.QDesktopWidget().availableGeometry().center()
+		qr.moveCenter(cp)
+		self.move(qr.topLeft())
+
+	def closeEvent(self, event):
+		""" Generates an event at closing prompting the user about closing the program """
+		reply = QtGui.QMessageBox.question(self, 'Message',
+			"Are you sure to quit?", QtGui.QMessageBox.Yes | 
+			QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+		if reply == QtGui.QMessageBox.Yes:
+			event.accept()
+		else:
+			event.ignore()
+
+	def keyPressEvent(self, e):
+		""" Exit the program using the 'ESC' key """
+		if e.key() == QtCore.Qt.Key_Escape:
+			self.close()
 
 def main():
-	
-	return 0
+    
+	app = QtGui.QApplication(sys.argv)
+	ex = EC()
+	ex.show()
+	sys.exit(app.exec_())
 
 if __name__ == '__main__':
 	main()
