8d4eca314cf73b3a5bea665207c35a368efbd67e
Diogo Silva <o.diogosilva@gmail.com>
2013-11-16 18:11:57 +0000

    Added new OutputFormatError exception

--- wingman/ErrorHandling.py
+++ wingman/ErrorHandling.py
@@ -26,3 +26,9 @@ class ArgumentError(Exception):
 		self.value = value
 	def __str__ (self):
 		return repr(self.value)
+
+class OutputFormatError(Exception):
+	def __init__ (self, value):
+		self.value = value
+	def __str__ (self):
+		return repr(self.value)
