a04d1a0b8ca6f5f0703ab60f036da8f3ee93578d
Diogo Silva <o.diogosilva@gmail.com>
2013-11-17 20:25:57 +0000

    Started implementation of a module to handle and filter missing data on alignments

---
+++ wingman/MissingFilter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  
+#  Copyright 2012 Unknown <diogo@arch>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+class MissingFilter ():
+	""" Contains several methods used to trim and filter missing data from alignments. It's mainly used for inheritance """
+
+	def __init__ (self, alignment_dict):
+
+		self.alignment = alignment_dict
+
+	def filter_terminals (self):
+		""" Given an alignment, this will replace the gaps in the extremities of the alignment with missing data """
